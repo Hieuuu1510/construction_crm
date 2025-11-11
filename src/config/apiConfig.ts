f93a515111ce2ebb881ae7fd4cda065b68c70dba
TranMinhHieu <hieuhienlanh03@gmail.com>
2025-11-11 23:35:09 +0700

    chore: setup redux & base APIClient

---
+++ src/config/apiConfig.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosInstance } from "axios";
+
+class BaseApi {
+  private axiosInstance: AxiosInstance;
+
+  constructor(baseUrl?: string) {
+    this.axiosInstance = axios.create({
+      baseURL: baseUrl || process.env.BACKEND_API_URL,
+    });
+  }
+
+  async GET<T = any>(url: string, params?: any, config?: any): Promise<T> {
+    return this.axiosInstance
+
+      .get(url, { params, ...(config as any) })
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err;
+      });
+  }
+
+  async POST<T = any>(url: string, data?: any, config?: any): Promise<T> {
+    return this.axiosInstance
+      .post(url, data, { ...config })
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err;
+      });
+  }
+}
+
+export { BaseApi };
+
+const ApiClient = new BaseApi();
+
+export default ApiClient;
